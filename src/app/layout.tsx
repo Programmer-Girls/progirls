a4e3622ad570a2c5b09b59c39f6af9088976e94b
categoricalcat <wallacefares@gmail.com>
2025-01-19 15:59:04 -0300

    chore: update dependencies and enhance layout
    - Updated 'tailwind-variants' to version 0.3.1 and 'eslint-plugin-prettier' to version 5.2.3 in package.json and package-lock.json.
    - Added 'xlsx' version 0.18.5 to package.json for Excel file handling capabilities.
    - Updated pnpm-lock.yaml to reflect the new versions and dependencies.
    - Modified layout.tsx to wrap children in a main container for improved layout structure.
    - Enhanced the 'comunidade' page layout for better responsiveness and visual appeal.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       >
         <Header />
 
-        {children}
+        <main className="container mx-auto">{children}</main>
 
         <Footer />
       </body>
