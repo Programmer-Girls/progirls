f9130de75dc0cb765a3c6861525ed3e04dcae902
Letycia <lelocha19@gmail.com>
2025-06-12 11:51:22 -0300

    Adiciona dados estáticos para tabela Integrante e imagens associadas

--- app.py
+++ app.py
@@ -34,6 +34,15 @@ with app.app_context():
 
     if not Integrante.query.first():
         print("Tabela 'integrante' criada e está vazia.")   
+        integrantes_para_adicionar = [
+            Integrante(Nome="Karin Abe", Bio="Voluntária de Moderação e Criação de Conteúdo", foto_url="image/karin.jpg", cargo="Moderadora"),
+            Integrante(Nome="Niedja Araújo", Bio="Voluntária de Moderação e Criação de Conteúdo", foto_url="image/niedja.jpg", cargo="Moderadora"),
+            Integrante(Nome="Mariana Aragão", Bio="Voluntária de Moderação e Criação de Conteúdo", foto_url="image/mari.jpg", cargo="Moderadora"),
+        ]
+
+        db.session.add_all(integrantes_para_adicionar)
+        db.session.commit()
+        print("Integrantes adicionados com sucesso!")
             
 
     # Função para Atualizar nome da Leticya se já existir com nome antigo
